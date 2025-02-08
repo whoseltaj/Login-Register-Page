244545ee7a3931ad98a319ec4a4052bc294f5503
Eltaj Abbasov <114187382+whoseltaj@users.noreply.github.com>
2025-02-08 13:55:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+let keyframes = ["wave-pear-effect"]
+// Normal GSAP animation.......
+gsap.from(".fruit-image ", { y: "-100vh", delay: 0.5 });
+gsap.to(".fruit-image img", {
+  x: "random(-20, 20)",
+  y: "random(-20, 20)",
+  zIndex: 22,
+  duration: 2,
+  ease: "none",
+  yoyo: true,
+  repeat: -1
+});
